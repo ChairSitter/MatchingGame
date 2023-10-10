282afe1e2fddb95fa20b3abb10294f0086147238
Bryan Schneller <bryan.schneller@gmail.com>
2023-10-10 14:10:39 -0400

    Change to correct runfunctions speed

--- roverPractice.js
+++ roverPractice.js
@@ -1617,7 +1617,7 @@ const tieBreak = () => {
 };*/
 
 //code for slightly faster boxes
-/*const runFunctions = () => {
+const runFunctions = () => {
     document.getElementById('input-button').disabled = true;
     document.getElementById('mult-button').disabled = false;
     multiplierCountdown();
@@ -1637,7 +1637,7 @@ const tieBreak = () => {
     setTimeout(() => {
         document.getElementById('mult-button').disabled = true;
     }, 7200);
-};*/
+};
 
 //code to alternate between which side is displayed first
 /*const runFunctions = () => {
@@ -1814,7 +1814,7 @@ const tieBreak = () => {
 };*/
 
 //test function that will call functions much faster
-const runFunctions = () => {
+/*const runFunctions = () => {
     document.getElementById('input-button').disabled = true;
     document.getElementById('mult-button').disabled = false;
     resetter();
@@ -1833,7 +1833,7 @@ const runFunctions = () => {
         setTimeout(() => {
         document.getElementById('mult-button').disabled = true;
     }, 800);
-}
+}*/
 
 //Sets the Play button to call the main function
 inputButton.onclick = runFunctions;
